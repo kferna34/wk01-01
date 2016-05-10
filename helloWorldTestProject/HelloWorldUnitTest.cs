ab580550627dc55358762166046c48e57ead4f41
kferna34 <kferna34@my.centennialcollege.ca>
2016-05-10 14:19:42 -0400

    added reference

--- helloWorldTestProject/HelloWorldUnitTest.cs
+++ helloWorldTestProject/HelloWorldUnitTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace helloWorldTestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class HelloWorldUnitTest
     {
         [TestMethod]
         public void TestMethod1()
